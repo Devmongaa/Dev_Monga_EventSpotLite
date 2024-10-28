0161bdf4c0604eb3caea0b8630580f72cfc96c55
Devmongaa <163189502+Devmongaa@users.noreply.github.com>
2024-10-28 22:24:12 +0530

    Update Login.jsx

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -70,7 +70,7 @@ export default function Login() {
                 </div>
                 <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
               </div>
-              <button type="submit" className="w-full text-white bg-slate-800 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+              <button type="submit" className="w-full text-white bg-indigo-800 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                 Sign in
               </button>
 
